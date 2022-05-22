786632aea346883bdc6960482809fb1904000cae
OlhaTsyhanenko <79564881+OlhaTsyhanenko@users.noreply.github.com>
2022-05-22 09:30:17 +0000

    Deploying to gh-pages from @ OlhaTsyhanenko/test-for-Tokarskyi@8b665fac9427ecbc6b26a442a3fe419da9ad13e8 🚀

--- src.3b5b933c.js
+++ src.3b5b933c.js
@@ -5,4 +5,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"modern-normalize/modern-normalize.css":"Y5dV"}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/test-for-Tokarskyi/src.2821fe1e.js.map
+//# sourceMappingURL=/test-for-Tokarskyi/src.3b5b933c.js.map
